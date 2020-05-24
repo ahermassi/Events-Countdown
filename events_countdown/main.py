20915b7f9342a7a35484c217c8ddcb6f36151487
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-05-24 13:49:00 -0400

    Define CLI arguments

--- events_countdown/main.py
+++ events_countdown/main.py
@@ -1,6 +1,16 @@
-def main():
-    print('Test')
+import click
 
 
-if __name__ == '__main__':
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('--add', '-a', is_flag=True, help="Add events.")
+@click.option('--delete', '-d', is_flag=True, help="Delete previously added events.")
+@click.option('--clean', '-c', is_flag=True, help="Delete expired events.")
+@click.option('--config', is_flag=True, help="Print config file path.")
+def main(add, delete=False, clean=False, config=False):
+
+    if add:
+        print('Hello from add')
+
+
+if __name__ == "__main__":
     main()
